e133631220b03f55d2f99dac82054cdae95e7891
Ricardo Augusto <ricardosmiguel@live.com>
2023-01-13 04:00:54 -0300

    Troca da variável

--- exercicios-python/ex035.py
+++ exercicios-python/ex035.py
@@ -1,8 +1,8 @@
-Comp1 = int(input('Digite o primeiro comprimento: '))
+Comp = int(input('Digite o primeiro comprimento: '))
 Comp2 = int(input('Digite o segundo comprimento: '))
 Comp3 = int(input('Digite o terceiro comprimento: '))
 
-if Comp1 + Comp2 > Comp3 and Comp1 + Comp3 > Comp2 and Comp2 + Comp3 > Comp1:
+if Comp + Comp2 > Comp3 and Comp + Comp3 > Comp2 and Comp2 + Comp3 > Comp:
     print('Essas retas formam um triângulo!')
 else:
     print('Essas retas NÃO formam um triângulo!')
